45e92f708c9427d3b978954280d66dd8d6f8151f
MinhKhanh188 <127714712+MinhKhanh188@users.noreply.github.com>
2024-04-05 02:54:13 +0700

    Add files via upload

---
+++ WishingProject/GachaSystem.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GachaSystem
+{
+    private List<Item> itemList;
+    private Random random;
+    private int pityCounter;
+
+    public GachaSystem()
+    {
+        itemList = new List<Item>();
+        random = new Random();
+        pityCounter = 0;
+
+        // Populate itemList with different items and their rarities
+        // Adjust the probabilities and item details based on your game design
+        itemList.Add(new Item("Gương ", Rarity.Common, 80));
+        itemList.Add(new Item("Bút chì ", Rarity.Common, 80));
+        itemList.Add(new Item("Figure Cùi ", Rarity.Rare, 15));
+        itemList.Add(new Item("Figure Gundam ", Rarity.Epic, 4));
+        itemList.Add(new Item("Chị Chủ Nhiệm", Rarity.Legendary, 1));
+    }
+
+    public (Item, Rarity) Pull()
+    {
+        pityCounter++;
+
+        // Check if the pity counter has reached a certain threshold to guarantee a higher rarity item
+        if (pityCounter >= 10)
+        {
+            pityCounter = 0;
+            return (GetRandomItemWithMinimumRarity(Rarity.Rare), Rarity.Rare);
+        }
+
+        Item obtainedItem = GetRandomItem();
+        return (obtainedItem, obtainedItem.ItemRarity);
+    }
+
+
+
+    private Item GetRandomItem()
+    {
+        int totalWeight = itemList.Sum(item => item.Probability);
+        int randomNumber = random.Next(0, totalWeight);
+
+        foreach (var item in itemList)
+        {
+            if (randomNumber < item.Probability)
+            {
+                return item;
+            }
+
+            randomNumber -= item.Probability;
+        }
+
+        // This should not happen under normal circumstances
+        throw new InvalidOperationException("Unable to select a random item.");
+    }
+
+    private Item GetRandomItemWithMinimumRarity(Rarity minRarity)
+    {
+        var eligibleItems = itemList.Where(item => item.ItemRarity >= minRarity).ToList();
+        return GetRandomItemFromList(eligibleItems);
+    }
+
+    private Item GetRandomItemFromList(List<Item> items)
+    {
+        int totalWeight = items.Sum(item => item.Probability);
+        int randomNumber = random.Next(0, totalWeight);
+
+        foreach (var item in items)
+        {
+            if (randomNumber < item.Probability)
+            {
+                return item;
+            }
+
+            randomNumber -= item.Probability;
+        }
+
+        // This should not happen under normal circumstances
+        throw new InvalidOperationException("Unable to select a random item from the specified list.");
+    }
+}
+
+public class Item
+{
+    public string Name { get; }
+    public Rarity ItemRarity { get; }
+    public int Probability { get; }
+
+    public Item(string name, Rarity rarity, int probability)
+    {
+        Name = name;
+        ItemRarity = rarity;
+        Probability = probability;
+    }
+}
+
+public enum Rarity
+{
+    Common,
+    Rare,
+    Epic,
+    Legendary
+}
---
+++ WishingProject/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WishingProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GachaSystem gachaSystem;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gachaSystem = new GachaSystem();
+        }
+
+        private void PullButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Trigger the meteor animation
+            PlayAnimation("MeteorAnimation");
+
+            // Call the GachaSystem logic here
+            (Item obtainedItem, Rarity rarity) = gachaSystem.Pull();
+
+            // Update the UI based on the obtained item
+            itemDisplay.Text = $"Obtained Item: {obtainedItem.Name} (Rarity: {obtainedItem.ItemRarity})";
+
+            // Play the corresponding animation based on rarity
+            PlayRarityAnimation(rarity);
+        }
+
+
+
+        private void PlayRarityAnimation(Rarity rarity)
+        {
+            Debug.WriteLine($"Playing {rarity} video.");
+            //MediaElement videoElement = (MediaElement)Resources[$"{rarity}Animation"];
+            
+                videoDisplay.LoadedBehavior = MediaState.Manual;
+
+                // Stop any currently playing video
+                videoDisplay.Stop();
+
+                // Set the source of the video display
+                videoDisplay.Source = new Uri($"pack://application:,,,/Material/WishingAnimation/Meteor/4starwish.mp4");
+
+                // Play the video
+                videoDisplay.Play();
+            
+        }
+
+
+        private void StopAllVideos()
+        {
+            // Stop all rarity videos
+            StopVideo("ThreeStarAnimation");
+            StopVideo("FourStarAnimation");
+            StopVideo("FiveStarAnimation");
+        }
+
+        private void StopVideo(string videoKey)
+        {
+            MediaElement videoElement = (MediaElement)Resources[videoKey];
+            if (videoElement != null)
+            {
+                videoElement.Stop();
+            }
+        }
+
+
+        private void PlayAnimation(string animationKey)
+        {
+            Storyboard animation = (Storyboard)Resources[animationKey];
+            if (animation != null)
+            {
+                meteor.BeginStoryboard(animation);
+            }
+
+        }
+    }
+}
